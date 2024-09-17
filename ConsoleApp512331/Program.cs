15d5dd2f234d52618aeb39e381434474b211e08f
EugenSigmaTailerDerden <kzena508@gmail.com>
2024-09-17 11:43:12 +0300

    пппуп

--- ConsoleApp512331/Program.cs
+++ ConsoleApp512331/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Я люблю пивзавод за його дорогу))))))))");
+﻿
+
+
+Console.WriteLine("Hello C# again!");
+
+// cw + TAB = Console.WriteLine();
+Console.WriteLine("This is text line...");
+
+// WriteLine - ставить новий рядок вкінці
+// Write - не ставить новий рядок після тексту
+
+// змінити колір фону та тексту
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Red");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Green");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Blue");
+
+// скинути кольори до стандартних
+Console.ResetColor();
+
+// спец символи: \n \t \' \"
+
+Console.Write("\nNew Line\tTab\t\tBye!!!\n\n");
+
+// отримати значення від користувача
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Скільки градусі на вулиці: ");
+int temperatures = int.Parse(Console.ReadLine());
+
+// -------------------------- урок 2
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World");
+
+Console.Write("Hello World");
+Console.Write("Hello World");
+Console.WriteLine();
+
+
+
+// типи даних:
+//  - string
+//  - int, float
+ //створення змінної: тип назва = значення;
+
+string username = "anonymous";
+username = "blabla";
+username = "no name";
+
+Console.Write("Сільки градусів на вулиі?????????????????????????????????????? ");
+username = Console.ReadLine();
+
+ //інтерполяція: $"...{вираз}..."
+Console.WriteLine($"Hello dear, {username}!");
+Console.WriteLine($"Ти має {temperatures * 1.8 + 32} фаренгейтів)");
+
+
+
+
+
+
+
+
